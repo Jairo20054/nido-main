40b6494ce541626ebd6bbcbef487071d8e68088c
Jairo20054 <castillojairo2001@gmail.com>
2025-10-08 01:38:05 +0000

    cambioscambios

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
- import { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import { useState, useEffect, useCallback, useMemo, useRef, useLayoutEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import {
